d5d8e4b749a78dc906b8a90aef70aa13cd02998c
Andre Tan <tanandre93@gmail.com>
2016-02-19 19:51:31 +0800

    Add flatly theme

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return render_template('index.html')
+    return render_template('dashboard.html')
 
 if __name__ == "__main__":
 	app.run()
