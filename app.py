279f7934c859e29ed7767be4d9c2341b7bc60338
Andre Tan <tanandre93@gmail.com>
2016-02-19 21:59:05 +0800

    Add new dics, extratrees directories and update app.py

--- app.py
+++ app.py
@@ -1,9 +1,34 @@
 from flask import Flask, render_template, url_for
+
+#internal libraries
+import itertools,csv,sys,os,pickle
+#External libraries
+import gmaps,tweepy,wgetter,numpy as np,pandas as pd
+from sklearn.externals import joblib
+from TrainClassifier import labelHash, separate_column_by_type, process_nontext
+import Compiled as cp
+from scipy import stats
+from geopy.geocoders import Nominatim
+import operator
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return render_template('dashboard.html')
+	pred = cp.predictTerroristGroup()
+	mult = cp.multipleAttacks(pred)
+	location = cp.typeFreqPlaceAttacked(pred)
+	casualties = cp.numOfCasualties(pred)
+	weaptype = cp.findTypeOfWeapon(pred)
+	propdmg = cp.findPropertyDamage(pred)
+	nperps = cp.numPerps(pred)
+	return render_template('dashboard.html',
+    	prediction=pred,
+    	location=location,
+    	mult_bool=mult,
+    	casualties_num=casualties,
+    	weaptype=weaptype,
+    	propdmg_prob=propdmg,
+    	numperps_arr=nperps)
 
 if __name__ == "__main__":
-	app.run()
+	app.run(debug=True)
