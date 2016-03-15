aeb4d75ba0b51e7007ac3d9520cbbefd9ca331e5
waffleboy <A0130737@nus.edu.sg>
2016-03-15 21:06:20 +0800

    adding csv button - unfinished

---
+++ static/js/button.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Thiru on 11-Mar-16.
+ */
+function upload() {
+    var x = document.createElement("INPUT");
+    x.setAttribute("type", "file");
+    document.body.appendChild(x);
+}
